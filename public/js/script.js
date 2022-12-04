762aa10924c0780fe07be575b56fe82931efea18
reginamarthaa <regina.535210040@stu.untar.ac.id>
2022-12-04 16:28:11 +0700

    greetings

--- public/js/script.js
+++ public/js/script.js
@@ -16,4 +16,28 @@ function scrollFunction() {
     }
 }
 
+// function showGreet(){
+//   var dt = new Date();
+//   var time = dt.getHours();
+
+//   if (time > 0 && time < 10) {
+//     $('#tampilsalam').html('<div>pagi</div>');
+//   }
+// }
+
+$(document).ready(function() {
+  var dt = new Date($.now());
+  var time = dt.getHours();
+
+  if (time > 0 && time < 10) {
+    $('#tampilsalam').html('<span class="text" >Selamat pagi,</span>');
+  } else if (time >= 10 && time < 15) {
+    $('#tampilsalam').html('<span class="text">Selamat siang,</span>'); 
+  } else if (time >= 15 && time < 19) {
+    $('#tampilsalam').html('<span class="text">Selamat sore,</span>'); 
+  }else if (time >= 19 && time < 24) {
+    $('#tampilsalam').html('<span class="text">Selamat malam,</span>'); 
+  }
+})
+
 $("#carouselExampleIndicators").carousel({interval: 2500});
